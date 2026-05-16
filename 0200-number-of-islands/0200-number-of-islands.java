d24d002e0b70ed76f45cfab51cb783c0eb4510e6
Harsh Shah <harsshah1994@gmail.com>
2026-05-16 15:17:10 -0400

    Time: 3 ms (86.81%), Space: 52.3 MB (46.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,12 +2,12 @@ class Solution {
     public int numIslands(char[][] grid) {
         
         int result = 0;
-        final Set<String> visited = new HashSet<>();
+
 
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[i].length; j++){
-                if(grid[i][j] == '1' && !visited.contains( i + "-" + j)){ //Recursively check left,right,top,bottom
-                    spanIsland(grid,i,j,visited);
+                if(grid[i][j] == '1'){ 
+                    spanIsland(grid,i,j);
                     result++;
                 }
             }
@@ -17,13 +17,13 @@ class Solution {
 
     }
 
-    public void spanIsland(char[][] grid, int row, int col, Set<String> visited){
-        if(row >= 0 && col >= 0 && row < grid.length && col < grid[row].length && !visited.contains(row + "-" + col) && grid[row][col] == '1'){
-            visited.add(row + "-" + col);
-            spanIsland(grid,row+1,col,visited);
-            spanIsland(grid,row-1,col,visited);
-            spanIsland(grid,row,col+1,visited);
-            spanIsland(grid,row,col-1,visited);
+    public void spanIsland(char[][] grid, int row, int col){
+        if(row >= 0 && col >= 0 && row < grid.length && col < grid[row].length && grid[row][col] == '1'){
+            grid[row][col] = '0';
+            spanIsland(grid,row+1,col);
+            spanIsland(grid,row-1,col);
+            spanIsland(grid,row,col+1);
+            spanIsland(grid,row,col-1);
         }
     }
 

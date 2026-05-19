6284ecee6555d9d827cc6195ef962d874ab66fdb
Harsh Shah <harsshah1994@gmail.com>
2026-05-19 07:00:16 -0400

    Time: 16 ms (26.52%), Space: 49.9 MB (36.42%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        final Map<FrequencyChar, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            FrequencyChar freqChar = new FrequencyChar(str);
+            List<String> result = map.getOrDefault(freqChar, new ArrayList<>());
+            result.add(str);
+            map.put(freqChar,result);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        return map.values().stream().collect(Collectors.toList());
+    }
+
+}
+
+class FrequencyChar {
+
+    char[] charArry = new char[26];
+    int hash;
+
+    public FrequencyChar(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            charArry[str.charAt(i) - 'a']++;
+            hash += str.charAt(i);
+        }
+
+    }
+
+    @Override
+    public int hashCode() {
+        return this.hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof FrequencyChar))
+            return false;
+
+        return Arrays.equals(this.charArry, ((FrequencyChar) obj).charArry);
+    }
+
+}

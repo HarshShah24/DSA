cb139560d1fcc1f11553320934bc6b5e38c07647
Harsh Shah <harsshah1994@gmail.com>
2026-05-19 09:19:42 -0400

    Time: 0 ms (100%), Space: 42.5 MB (85.34%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,30 +1,12 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length()){
+        
+       if(s.length() != goal.length()){
             return false;
-        }
+       }
+    
+    String concat = s + s;
+    return concat.contains(goal);
 
-        final Map<Character,List<Integer>> charIndexMap = new HashMap<>();
-
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            charIndexMap.computeIfAbsent(ch, k -> new ArrayList<>()).add(i);
-        }
-
-        char ch = goal.charAt(0);
-
-        if(!charIndexMap.containsKey(ch)){
-            return false;
-        }
-
-        for(int index : charIndexMap.get(ch)){
-            String preString = s.substring(0,index);
-            String postString = s.substring(index);
-            if((postString + preString).equals(goal)){
-                return true;
-            }
-        }
-
-        return false;
     }
 }
